d4541febf7b67e54590c2c8e7c4852f080076e6c
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-05-03 16:49:41 +0200

    directs to right room

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -2,7 +2,18 @@ import React, {useState, useEffect } from 'react'
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
-const ChatRoom = () => {
+const ChatRoom = ({ location }) => {
+
+      const [name, setName] = useState("");
+      const [room, setRoom] = useState("");
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+        setName(name);
+        setRoom(room);
+    });
+
+
     return (
         <div>
             <h1>ChatRoom</h1>
